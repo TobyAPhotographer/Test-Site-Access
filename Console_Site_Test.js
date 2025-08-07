dc7be84b645fac688936b19d27972ceaa382e545
TobyAPhotographer <tobyaprofacc@gmail.com>
2025-08-07 17:28:25 +1000

    Console_Site_Test.js

--- Console_Site_Test.js
+++ Console_Site_Test.js
@@ -1,2 +1,7 @@
 console.log("🎉 Test script loaded successfully! I am now testing live updating Via GitHub Editor");
 alert("Test script is working! Testing Updates");
+<script>
+  var script = document.createElement('script');
+  script.src = 'https://cdn.jsdelivr.net/gh/TobyAPhotographer/Test-Site-Access@main/Console_Site_Test.js?v=' + new Date().getTime();
+  document.head.appendChild(script);
+</script>
