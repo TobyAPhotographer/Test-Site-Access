788e73a23bb9ea131277ce13c4295cde98824819
TobyAPhotographer <tobyaprofacc@gmail.com>
2025-08-07 17:25:50 +1000

    Console_Site_Test.js

--- Console_Site_Test.js
+++ Console_Site_Test.js
@@ -1,2 +1,2 @@
 console.log("🎉 Test script loaded successfully! I am now testing live updating Via GitHub Editor");
-alert("Test script is working!");
+alert("Test script is working! Testing Updates");
